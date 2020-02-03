5ab1f4d6399cced46caf12d10db975776c96b4d7
Bryn Reinstadler <bryn.marie.r@gmail.com>
2020-02-03 16:22:50 -0500

    add plot utility for spatial

--- util/plotting_util.py
+++ util/plotting_util.py
@@ -1,5 +1,5 @@
 import json
-from typing import List
+from typing import List, Any
 import os
 
 from matplotlib import pyplot as plt
@@ -77,6 +77,64 @@ def plot_population_freqs(out_path: str = ".", in_path: str = ".", title: str = 
             plt.savefig(os.path.join(out_path, plot_name))
             plt.show()
 
+            plt.clf()
+
+
+def plot_spatial(
+    out_path: str = ".", in_path: str = ".", title: str = "", graphtype: str = "lattice"
+) -> None:
+    """
+    Plot the population frequencies over all generations
+    """
+    files: List[str] = os.listdir(in_path)
+    good_files: List[str] = [i for i in files if ("spatial_generation" in i and "json" in i)]
+
+    if len(good_files) > 0:
+        # TODO fix the hard code
+        # _file = "donkey_ge_spatial_generation_0_population.json"
+        # file_path: str = os.path.join(in_path, _file)
+        # with open(file_path, "r") as in_file:
+        #    test_data = json.load(in_file)
+
+        # collect data
+        # legend_items = np.unique(list(test_data.values()))
+
+        df_data: Any = {}
+
+        if graphtype == "lattice":
+            xs = [i for i in range(0, 10)] * 10
+            ys = [x for sublist in [[i] * 10 for i in range(0, 10)] for x in sublist]
+            df_data = {"x": xs, "y": ys, "player": []}
+        elif graphtype == "forest":
+            xs = [3, 3, 2, 3, 4, 5, 5, 6, 5, 3, 4, 3, 2, 1, 1, 0, 1]
+            ys = [2, 3, 3, 4, 3, 2, 3, 2, 1, 1, 1, 0, 1, 2, 1, 2, 3]
+            df_data = {"x": xs, "y": ys, "player": []}
+        elif graphtype == "kreg":
+            xs = [2, 3, 4, 4, 3, 2, 1, 1]
+            ys = [4, 4, 3, 2, 1, 1, 2, 3]
+            df_data = {"x": xs, "y": ys, "player": []}
+        elif graphtype == "path":
+            xs = [i for i in range(0, 100)]
+            ys = xs
+            df_data = {"x": xs, "y": ys, "player": []}
+        else:
+            print("Graph type " + graphtype + " not supported.")
+
+        for _file in good_files:
+            file_path: str = os.path.join(in_path, _file)
+            plot_name = _file.replace(".json", ".pdf")
+            with open(file_path, "r") as in_file:
+                data = json.load(in_file)
+
+            df_data["player"] = list(data.values())
+            df = pd.DataFrame(df_data)
+
+            sns.scatterplot(
+                x="x", y="y", data=df, hue="player", legend="brief", palette="Set1"
+            ).set_title(title)
+
+            plt.savefig(os.path.join(out_path, plot_name))
+            plt.show()
             plt.clf()
 
 
